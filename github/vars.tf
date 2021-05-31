e3050fe91cba4c700dc8bf9beac64eefc8a9d722
Mayank Pathak <onlinejudge95@gmail.com>
2021-05-31 23:03:50 +0530

    Adds variables for github org secrets

--- github/vars.tf
+++ github/vars.tf
@@ -3,3 +3,29 @@ variable "GH_PERSONAL_ACCESS_TOKEN" {
   description = "Personal Access Token for GitHub"
   sensitive   = true
 }
+
+
+variable "DOCKERHUB_USERNAME" {
+  type = string
+  description = "Dockerhub username for login"
+}
+
+
+variable "DOCKERHUB_TOKEN" {
+  type = string
+  description = "Dockerhub token for login"
+  sensitive   = true
+}
+
+
+variable "HEROKU_EMAIL" {
+  type = string
+  description = "Heroku email for login"
+}
+
+
+variable "HEROKU_API_KEY" {
+  type = string
+  description = "Heroku API key for deployment"
+  sensitive   = true
+}

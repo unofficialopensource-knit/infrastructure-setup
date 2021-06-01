237b9dd86be46bfa49097dee9c79adaffe060516
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-01 14:26:36 +0530

    Formats code for actions

--- github/github_actions_secret.tf
+++ github/github_actions_secret.tf
@@ -1,10 +1,10 @@
-resource "github_actions_secret" "codecov_token_email_service" {
+resource "github_actions_secret" "email_service_codecov_token" {
   repository      = "email_service"
   secret_name     = "CODECOV_TOKEN"
   plaintext_value = var.EMAIL_SERVICE_CODECOV_TOKEN
 }
 
-resource "github_actions_secret" "codecov_token_api_gateway" {
+resource "github_actions_secret" "api_gateway_codecov_token" {
   repository      = "api_gateway"
   secret_name     = "CODECOV_TOKEN"
   plaintext_value = var.API_GATEWAY_CODECOV_TOKEN

58510e459284022d7f1ee80995f475ea3484e80c
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-01 21:53:24 +0530

    Format commit

--- github/github_branch_default.tf
+++ github/github_branch_default.tf
@@ -1,29 +1,29 @@
 resource "github_branch_default" "api_gateway_default_branch" {
-    repository = github_repository.api_gateway.name
-    branch = github_branch.api_gateway_release.branch
+  repository = github_repository.api_gateway.name
+  branch     = github_branch.api_gateway_release.branch
 }
 
 resource "github_branch_default" "email_service_default_branch" {
-    repository = github_repository.email_service.name
-    branch = github_branch.email_service_release.branch
+  repository = github_repository.email_service.name
+  branch     = github_branch.email_service_release.branch
 }
 
 resource "github_branch_default" "platform_setup_default_branch" {
-    repository = github_repository.platform_setup.name
-    branch = github_branch.platform_setup_main.branch
+  repository = github_repository.platform_setup.name
+  branch     = github_branch.platform_setup_main.branch
 }
 
 resource "github_branch_default" "user_management_service_default_branch" {
-    repository = github_repository.user_management_service.name
-    branch = github_branch.user_management_service_release.branch
+  repository = github_repository.user_management_service.name
+  branch     = github_branch.user_management_service_release.branch
 }
 
 resource "github_branch_default" "web_client_default_branch" {
-    repository = github_repository.web_client.name
-    branch = github_branch.web_client_release.branch
+  repository = github_repository.web_client.name
+  branch     = github_branch.web_client_release.branch
 }
 
 resource "github_branch_default" "wiki_default_branch" {
-    repository = github_repository.wiki.name
-    branch = github_branch.wiki_main.branch
+  repository = github_repository.wiki.name
+  branch     = github_branch.wiki_main.branch
 }

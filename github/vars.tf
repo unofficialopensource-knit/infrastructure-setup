0c83f8fe3be361d87c2dffef1fed25f26496af8e
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-01 13:05:58 +0530

    Formats env vars for setting secrets

--- github/vars.tf
+++ github/vars.tf
@@ -4,28 +4,36 @@ variable "GH_PERSONAL_ACCESS_TOKEN" {
   sensitive   = true
 }
 
-
 variable "DOCKERHUB_USERNAME" {
-  type = string
+  type        = string
   description = "Dockerhub username for login"
 }
 
-
 variable "DOCKERHUB_TOKEN" {
-  type = string
+  type        = string
   description = "Dockerhub token for login"
   sensitive   = true
 }
 
-
 variable "HEROKU_EMAIL" {
-  type = string
+  type        = string
   description = "Heroku email for login"
 }
 
-
 variable "HEROKU_API_KEY" {
-  type = string
+  type        = string
   description = "Heroku API key for deployment"
+  sensitive   = true
+}
+
+variable "EMAIL_SERVICE_CODECOV_TOKEN" {
+  type        = string
+  description = "Codecov token for email service"
+  sensitive   = true
+}
+
+variable "API_GATEWAY_CODECOV_TOKEN" {
+  type        = string
+  description = "Codecov token for API gateway"
   sensitive   = true
 }

122f82371ccb732c2a16602f4ed9e54812f89975
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-01 19:52:45 +0530

    Imports web_client repo in terraform

--- github/github_repository.tf
+++ github/github_repository.tf
@@ -93,6 +93,22 @@ resource "github_repository" "user_management_service" {
   auto_init              = false
 }
 
+resource "github_repository" "web_client" {
+  name                   = "web_client"
+  description            = "Codebase for web_client"
+  visibility             = "public"
+  has_issues             = true
+  has_projects           = false
+  has_wiki               = false
+  is_template            = false
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_rebase_merge     = false
+  delete_branch_on_merge = true
+  has_downloads          = true
+  auto_init              = false
+}
+
 resource "github_repository" "wiki" {
   name                   = "wiki"
   description            = "Codebase for wiki"

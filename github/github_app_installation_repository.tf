680029274c151b147826d4f6a0d01f931798c9f9
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-01 23:10:26 +0530

    Adds codefactor app integration for repos

--- github/github_app_installation_repository.tf
+++ github/github_app_installation_repository.tf
@@ -1,19 +1,44 @@
 resource "github_app_installation_repository" "api_gateway_codecov_app" {
-    installation_id = "16927671"
-    repository = github_repository.api_gateway.name
+  installation_id = "16927671"
+  repository      = github_repository.api_gateway.name
 }
 
 resource "github_app_installation_repository" "email_service_codecov_app" {
-    installation_id = "16927671"
-    repository = github_repository.email_service.name
+  installation_id = "16927671"
+  repository      = github_repository.email_service.name
 }
 
 resource "github_app_installation_repository" "user_management_service_codecov_app" {
-    installation_id = "16927671"
-    repository = github_repository.user_management_service.name
+  installation_id = "16927671"
+  repository      = github_repository.user_management_service.name
 }
 
 resource "github_app_installation_repository" "web_client_codecov_app" {
-    installation_id = "16927671"
-    repository = github_repository.web_client.name
+  installation_id = "16927671"
+  repository      = github_repository.web_client.name
+}
+
+resource "github_app_installation_repository" "api_gateway_codefactor_app" {
+  installation_id = "16967095"
+  repository      = github_repository.api_gateway.name
+}
+
+resource "github_app_installation_repository" "email_service_codefactor_app" {
+  installation_id = "16967095"
+  repository      = github_repository.email_service.name
+}
+
+resource "github_app_installation_repository" "platform_setup_codefactor_app" {
+  installation_id = "16967095"
+  repository      = github_repository.platform_setup.name
+}
+
+resource "github_app_installation_repository" "user_management_service_codefactor_app" {
+  installation_id = "16967095"
+  repository      = github_repository.user_management_service.name
+}
+
+resource "github_app_installation_repository" "web_client_codefactor_app" {
+  installation_id = "16967095"
+  repository      = github_repository.web_client.name
 }

4f3b86da52bc6f1017e3168f51bd870a0cdbd7c7
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-01 13:21:56 +0530

    Adds github repo config file

---
+++ github/github_repository.tf
@@ -0,0 +1,31 @@
+resource "github_repository" "auth_service" {
+  name                   = "auth_service"
+  description            = "Codebase for auth service"
+  visibility             = "public"
+  has_issues             = true
+  has_projects           = false
+  has_wiki               = false
+  is_template            = false
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_rebase_merge     = false
+  delete_branch_on_merge = true
+  has_downloads          = true
+  auto_init              = false
+}
+
+resource "github_repository" "bet_service" {
+  name                   = "bet_service"
+  description            = "Codebase for bet service"
+  visibility             = "public"
+  has_issues             = true
+  has_projects           = false
+  has_wiki               = false
+  is_template            = false
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_rebase_merge     = false
+  delete_branch_on_merge = true
+  has_downloads          = true
+  auto_init              = false
+}

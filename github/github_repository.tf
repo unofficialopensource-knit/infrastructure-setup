93e1ff9cd248f7b8b6741588e4e7e75e1999bf87
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-01 18:30:13 +0530

    Imports email_service repo in terraform

--- github/github_repository.tf
+++ github/github_repository.tf
@@ -29,3 +29,19 @@ resource "github_repository" "bet_service" {
   has_downloads          = true
   auto_init              = false
 }
+
+resource "github_repository" "email_service" {
+  name                   = "email_service"
+  description            = "Codebase for email service"
+  visibility             = "public"
+  has_issues             = true
+  has_projects           = false
+  has_wiki               = false
+  is_template            = false
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_rebase_merge     = false
+  delete_branch_on_merge = true
+  has_downloads          = true
+  auto_init              = false
+}

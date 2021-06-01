fa779b11bd96b56bd511d49416fb44a1975582e8
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-01 22:04:25 +0530

    Add branch protection rule for release & main branch in email_service

--- github/github_branch_protection.tf
+++ github/github_branch_protection.tf
@@ -33,3 +33,39 @@ resource "github_branch_protection" "api_gateway_branch_protection_release" {
     required_approving_review_count = 1
   }
 }
+
+resource "github_branch_protection" "email_service_branch_protection_main" {
+  repository_id          = github_repository.email_service.node_id
+  pattern                = "main"
+  enforce_admins         = false
+  require_signed_commits = false
+  required_status_checks {
+    strict = true
+    contexts = [
+      "pyup.io/safety-ci",
+      "lint",
+      "test",
+      "build"
+    ]
+  }
+}
+
+resource "github_branch_protection" "email_service_branch_protection_release" {
+  repository_id          = github_repository.email_service.node_id
+  pattern                = "release"
+  enforce_admins         = false
+  require_signed_commits = true
+  required_status_checks {
+    strict = true
+    contexts = [
+      "pyup.io/safety-ci",
+      "lint",
+      "test",
+      "build"
+    ]
+  }
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    required_approving_review_count = 1
+  }
+}

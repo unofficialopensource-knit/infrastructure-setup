119abd3c67baab24c0507ab21b04069ef9ecc815
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-01 19:50:16 +0530

    Formats tf file

--- github/github_repository.tf
+++ github/github_repository.tf
@@ -1,19 +1,3 @@
-resource "github_repository" "email_service" {
-  name                   = "email_service"
-  description            = "Codebase for email_service"
-  visibility             = "public"
-  has_issues             = true
-  has_projects           = false
-  has_wiki               = false
-  is_template            = false
-  allow_merge_commit     = false
-  allow_squash_merge     = true
-  allow_rebase_merge     = false
-  delete_branch_on_merge = true
-  has_downloads          = true
-  auto_init              = false
-}
-
 resource "github_repository" "api_gateway" {
   name                   = "api_gateway"
   description            = "Codebase for api_gateway"
@@ -30,9 +14,40 @@ resource "github_repository" "api_gateway" {
   auto_init              = false
 }
 
-resource "github_repository" "wiki" {
-  name                   = "wiki"
-  description            = "Codebase for wiki"
+resource "github_repository" "auth_service" {
+  name                   = "auth_service"
+  description            = "Codebase for auth service"
+  visibility             = "public"
+  has_issues             = true
+  has_projects           = false
+  has_wiki               = false
+  is_template            = false
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_rebase_merge     = false
+  delete_branch_on_merge = true
+  has_downloads          = true
+  auto_init              = false
+}
+resource "github_repository" "bet_service" {
+  name                   = "bet_service"
+  description            = "Codebase for bet service"
+  visibility             = "public"
+  has_issues             = true
+  has_projects           = false
+  has_wiki               = false
+  is_template            = false
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_rebase_merge     = false
+  delete_branch_on_merge = true
+  has_downloads          = true
+  auto_init              = false
+}
+
+resource "github_repository" "email_service" {
+  name                   = "email_service"
+  description            = "Codebase for email_service"
   visibility             = "public"
   has_issues             = true
   has_projects           = false
@@ -78,25 +93,9 @@ resource "github_repository" "user_management_service" {
   auto_init              = false
 }
 
-resource "github_repository" "auth_service" {
-  name                   = "auth_service"
-  description            = "Codebase for auth service"
-  visibility             = "public"
-  has_issues             = true
-  has_projects           = false
-  has_wiki               = false
-  is_template            = false
-  allow_merge_commit     = false
-  allow_squash_merge     = true
-  allow_rebase_merge     = false
-  delete_branch_on_merge = true
-  has_downloads          = true
-  auto_init              = false
-}
-
-resource "github_repository" "bet_service" {
-  name                   = "bet_service"
-  description            = "Codebase for bet service"
+resource "github_repository" "wiki" {
+  name                   = "wiki"
+  description            = "Codebase for wiki"
   visibility             = "public"
   has_issues             = true
   has_projects           = false

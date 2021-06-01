43b60a9d87b87e5026e577793504a7914085754e
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-01 21:57:32 +0530

    Add branch protection rule for release branch in api_gateway

--- github/github_branch_protection.tf
+++ github/github_branch_protection.tf
@@ -1,17 +1,35 @@
 resource "github_branch_protection" "api_gateway_branch_protection_main" {
-  repository_id = github_repository.api_gateway.node_id
-  pattern       = "main"
-  enforce_admins = false
+  repository_id          = github_repository.api_gateway.node_id
+  pattern                = "main"
+  enforce_admins         = false
   require_signed_commits = false
   required_status_checks {
-      strict = true
-      contexts = [
-          "pyup.io/safety-ci",
-          "lint",
-          "test",
-          "build"
-      ]
+    strict = true
+    contexts = [
+      "pyup.io/safety-ci",
+      "lint",
+      "test",
+      "build"
+    ]
   }
 }
 
-# resource "github_branch_protection" "api_gateway_branch_protection_release" {}
+resource "github_branch_protection" "api_gateway_branch_protection_release" {
+  repository_id          = github_repository.api_gateway.node_id
+  pattern                = "release"
+  enforce_admins         = false
+  require_signed_commits = true
+  required_status_checks {
+    strict = true
+    contexts = [
+      "pyup.io/safety-ci",
+      "lint",
+      "test",
+      "build"
+    ]
+  }
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    required_approving_review_count = 1
+  }
+}

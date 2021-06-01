9057c986853faba3e39df1193a154ea633a65db3
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-01 20:57:01 +0530

    Adds github_branch_default for email_service

--- github/github_branch_default.tf
+++ github/github_branch_default.tf
@@ -2,3 +2,8 @@ resource "github_branch_default" "api_gateway_default_branch" {
     repository = github_repository.api_gateway.name
     branch = github_branch.api_gateway_release.branch
 }
+
+resource "github_branch_default" "email_service_default_branch" {
+    repository = github_repository.email_service.name
+    branch = github_branch.email_service_release.branch
+}

1b58c370215af3972569ae78590366a5df488ba8
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-02 15:04:36 +0530

    Adds memebers for the org

---
+++ github/github_membership.tf
@@ -0,0 +1,9 @@
+resource "github_membership" "onlinejudge95_invite" {
+    username = "onlinejudge95"
+    role = "admin"
+}
+
+resource "github_membership" "pandeysambhi_invite" {
+    username = "pandeysambhi"
+    role = "member"
+}

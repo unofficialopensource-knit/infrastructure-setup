9ba9ffb8642d035f0e1cee241e3f6f80de75d97a
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2021-06-02 16:35:31 +0530

    Adds tem membership (#14)

---
+++ github/github_team_membership.tf
@@ -0,0 +1,23 @@
+resource "github_team_membership" "onlinejudge95_back_end_membership" {
+  team_id  = github_team.backend_team.id
+  username = "onlinejudge95"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "onlinejudge95_front_end_membership" {
+  team_id  = github_team.frontend_team.id
+  username = "onlinejudge95"
+  role     = "member"
+}
+
+resource "github_team_membership" "onlinejudge95_ops_membership" {
+  team_id  = github_team.ops_team.id
+  username = "onlinejudge95"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "pandeysambhi_front_end_membership" {
+  team_id  = github_team.frontend_team.id
+  username = "pandeysambhi"
+  role     = "maintainer"
+}

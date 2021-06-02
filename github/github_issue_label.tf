6503cdca27771088b2794919e5cdaf49666d0736
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-02 14:22:00 +0530

    Imports labels from platform_setup repo

--- github/github_issue_label.tf
+++ github/github_issue_label.tf
@@ -123,3 +123,66 @@ resource "github_issue_label" "email_service_wontfix" {
   color       = "ffffff"
   description = "This will not be worked on"
 }
+
+resource "github_issue_label" "platform_setup_bug" {
+  repository  = github_repository.platform_setup.name
+  name        = "bug"
+  color       = "d73a4a"
+  description = "Something isn't working"
+}
+
+resource "github_issue_label" "platform_setup_documentation" {
+  repository  = github_repository.platform_setup.name
+  name        = "documentation"
+  color       = "0075ca"
+  description = "Improvements or additions to documentation"
+}
+
+resource "github_issue_label" "platform_setup_duplicate" {
+  repository  = github_repository.platform_setup.name
+  name        = "duplicate"
+  color       = "cfd3d7"
+  description = "This issue or pull request already exists"
+}
+
+resource "github_issue_label" "platform_setup_enhancement" {
+  repository  = github_repository.platform_setup.name
+  name        = "enhancement"
+  color       = "a2eeef"
+  description = "New feature or request"
+}
+
+resource "github_issue_label" "platform_setup_good_first_issue" {
+  repository  = github_repository.platform_setup.name
+  name        = "good first issue"
+  color       = "7057ff"
+  description = "Good for newcomers"
+}
+
+resource "github_issue_label" "platform_setup_help_wanted" {
+  repository  = github_repository.platform_setup.name
+  name        = "help wanted"
+  color       = "008672"
+  description = "Extra attention is needed"
+}
+
+resource "github_issue_label" "platform_setup_invalid" {
+  repository  = github_repository.platform_setup.name
+  name        = "invalid"
+  color       = "e4e669"
+  description = "This doesn't seem right"
+}
+
+resource "github_issue_label" "platform_setup_question" {
+  repository  = github_repository.platform_setup.name
+  name        = "question"
+  color       = "d876e3"
+  description = "Further information is requested"
+}
+
+resource "github_issue_label" "platform_setup_wontfix" {
+  repository  = github_repository.platform_setup.name
+  name        = "wontfix"
+  color       = "ffffff"
+  description = "This will not be worked on"
+}

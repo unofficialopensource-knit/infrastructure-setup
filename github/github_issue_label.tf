6f0457f94377cb2f2aced90a91a896f4337f3bdf
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-02 14:30:36 +0530

    Imports labels from web_client repo

--- github/github_issue_label.tf
+++ github/github_issue_label.tf
@@ -249,3 +249,66 @@ resource "github_issue_label" "user_management_service_wontfix" {
   color       = "ffffff"
   description = "This will not be worked on"
 }
+
+resource "github_issue_label" "web_client_bug" {
+  repository  = github_repository.web_client.name
+  name        = "bug"
+  color       = "d73a4a"
+  description = "Something isn't working"
+}
+
+resource "github_issue_label" "web_client_documentation" {
+  repository  = github_repository.web_client.name
+  name        = "documentation"
+  color       = "0075ca"
+  description = "Improvements or additions to documentation"
+}
+
+resource "github_issue_label" "web_client_duplicate" {
+  repository  = github_repository.web_client.name
+  name        = "duplicate"
+  color       = "cfd3d7"
+  description = "This issue or pull request already exists"
+}
+
+resource "github_issue_label" "web_client_enhancement" {
+  repository  = github_repository.web_client.name
+  name        = "enhancement"
+  color       = "a2eeef"
+  description = "New feature or request"
+}
+
+resource "github_issue_label" "web_client_good_first_issue" {
+  repository  = github_repository.web_client.name
+  name        = "good first issue"
+  color       = "7057ff"
+  description = "Good for newcomers"
+}
+
+resource "github_issue_label" "web_client_help_wanted" {
+  repository  = github_repository.web_client.name
+  name        = "help wanted"
+  color       = "008672"
+  description = "Extra attention is needed"
+}
+
+resource "github_issue_label" "web_client_invalid" {
+  repository  = github_repository.web_client.name
+  name        = "invalid"
+  color       = "e4e669"
+  description = "This doesn't seem right"
+}
+
+resource "github_issue_label" "web_client_question" {
+  repository  = github_repository.web_client.name
+  name        = "question"
+  color       = "d876e3"
+  description = "Further information is requested"
+}
+
+resource "github_issue_label" "web_client_wontfix" {
+  repository  = github_repository.web_client.name
+  name        = "wontfix"
+  color       = "ffffff"
+  description = "This will not be worked on"
+}

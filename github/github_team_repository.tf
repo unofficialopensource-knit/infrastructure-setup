7fb2b7d5a6461777996e63d7bf07ad68dc7a00c4
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2021-06-02 16:53:19 +0530

    Adds team access on repos (#15)

---
+++ github/github_team_repository.tf
@@ -0,0 +1,35 @@
+resource "github_team_repository" "api_gateway_backend_team" {
+    team_id = github_team.backend_team.id
+    repository = github_repository.api_gateway.name
+    permission = "maintain"
+}
+
+resource "github_team_repository" "email_service_backend_team" {
+    team_id = github_team.backend_team.id
+    repository = github_repository.email_service.name
+    permission = "maintain"
+}
+
+resource "github_team_repository" "platform_setup_ops_team" {
+    team_id = github_team.ops_team.id
+    repository = github_repository.platform_setup.name
+    permission = "maintain"
+}
+
+resource "github_team_repository" "user_management_service_backend_team" {
+    team_id = github_team.backend_team.id
+    repository = github_repository.user_management_service.name
+    permission = "maintain"
+}
+
+resource "github_team_repository" "platform_setup_frontend_team" {
+    team_id = github_team.frontend_team.id
+    repository = github_repository.web_client.name
+    permission = "maintain"
+}
+
+resource "github_team_repository" "wiki_ops_team" {
+    team_id = github_team.ops_team.id
+    repository = github_repository.wiki.name
+    permission = "maintain"
+}

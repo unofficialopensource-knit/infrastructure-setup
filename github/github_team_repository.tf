886c65c4ca6b7b3302c1ab55ab8cc7a5a1a4c9a2
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-02 23:55:14 +0530

    Format commit

--- github/github_team_repository.tf
+++ github/github_team_repository.tf
@@ -1,48 +1,48 @@
 resource "github_team_repository" "api_gateway_backend_team" {
-    team_id = github_team.backend_team.id
-    repository = github_repository.api_gateway.name
-    permission = "maintain"
+  team_id    = github_team.backend_team.id
+  repository = github_repository.api_gateway.name
+  permission = "maintain"
 }
 
 resource "github_team_repository" "email_service_backend_team" {
-    team_id = github_team.backend_team.id
-    repository = github_repository.email_service.name
-    permission = "maintain"
+  team_id    = github_team.backend_team.id
+  repository = github_repository.email_service.name
+  permission = "maintain"
 }
 
 resource "github_team_repository" "platform_setup_ops_team" {
-    team_id = github_team.ops_team.id
-    repository = github_repository.platform_setup.name
-    permission = "maintain"
+  team_id    = github_team.ops_team.id
+  repository = github_repository.platform_setup.name
+  permission = "maintain"
 }
 
 resource "github_team_repository" "user_management_service_backend_team" {
-    team_id = github_team.backend_team.id
-    repository = github_repository.user_management_service.name
-    permission = "maintain"
+  team_id    = github_team.backend_team.id
+  repository = github_repository.user_management_service.name
+  permission = "maintain"
 }
 
 resource "github_team_repository" "web_client_frontend_team" {
-    team_id = github_team.frontend_team.id
-    repository = github_repository.web_client.name
-    permission = "maintain"
+  team_id    = github_team.frontend_team.id
+  repository = github_repository.web_client.name
+  permission = "maintain"
 }
 
 resource "github_team_repository" "wiki_ops_team" {
-    team_id = github_team.ops_team.id
-    repository = github_repository.wiki.name
-    permission = "maintain"
+  team_id    = github_team.ops_team.id
+  repository = github_repository.wiki.name
+  permission = "maintain"
 }
 
 resource "github_team_repository" "wiki_backend_team" {
-    team_id = github_team.backend_team.id
-    repository = github_repository.wiki.name
-    permission = "push"
+  team_id    = github_team.backend_team.id
+  repository = github_repository.wiki.name
+  permission = "push"
 }
 
 
 resource "github_team_repository" "wiki_frontend_team" {
-    team_id = github_team.frontend_team.id
-    repository = github_repository.wiki.name
-    permission = "push"
+  team_id    = github_team.frontend_team.id
+  repository = github_repository.wiki.name
+  permission = "push"
 }

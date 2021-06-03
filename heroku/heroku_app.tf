972cf2c972f7c2c8fc9af2d96befb350daef03a1
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-03 11:16:11 +0530

    Imports email_service into terraform state

--- heroku/heroku_app.tf
+++ heroku/heroku_app.tf
@@ -11,13 +11,26 @@ resource "heroku_app" "api_gateway_app" {
     }
 }
 
-# resource "heroku_app" "email_service_app" {
-#     name = "email-service-web"
-#     region = "us"
-#     stack = "container"
-#     config_vars = {}
-#     sensitive_config_vars = {}
-# }
+resource "heroku_app" "email_service_app" {
+    name = "email-service-web"
+    region = "us"
+    stack = "container"
+    config_vars = {
+        DB_REVISION = "001"
+        DEPLOYMENT_PLATFORM = "heroku"
+        FLASK_APP = "src.app:create_app"
+        FLASK_DEBUG = false
+        FLASK_ENV = "production"
+        PROCESS_TYPE = "web"
+    }
+    sensitive_config_vars = {
+        DATABASE_URL = var.EMAIL_SERVICE_DATABASE_URL
+        MAIL_PASSWORD = var.EMAIL_SERVICE_MAIL_PASSWORD
+        MAIL_PASSWORD = var.EMAIL_SERVICE_MAIL_USERNAME
+        REDIS_TLS_URL = var.EMAIL_SERVICE_REDIS_TLS_URL
+        REDIS_URL = var.EMAIL_SERVICE_REDIS_URL
+    }
+}
 
 # resource "heroku_app" "user_management_service_app" {
 #     name = "user-management-service-web"

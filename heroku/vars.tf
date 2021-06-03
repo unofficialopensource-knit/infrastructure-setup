80ae20ecb80d2520c3a906df259915d0ca9442f3
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-03 11:15:53 +0530

    Adds config vars for email_service

--- heroku/vars.tf
+++ heroku/vars.tf
@@ -8,3 +8,33 @@ variable "HEROKU_API_KEY" {
   description = "Heroku API key for deployment"
   sensitive   = true
 }
+
+variable "EMAIL_SERVICE_DATABASE_URL" {
+  type        = string
+  description = "Database URI for Email Service"
+  sensitive   = true
+}
+
+variable "EMAIL_SERVICE_MAIL_PASSWORD" {
+  type        = string
+  description = "Email password for Email Service"
+  sensitive   = true
+}
+
+variable "EMAIL_SERVICE_MAIL_USERNAME" {
+  type        = string
+  description = "Email address for Email Service"
+  sensitive   = true
+}
+
+variable "EMAIL_SERVICE_REDIS_TLS_URL" {
+  type        = string
+  description = "Redis(secure) URI for Email Service"
+  sensitive   = true
+}
+
+variable "EMAIL_SERVICE_REDIS_URL" {
+  type        = string
+  description = "Redis URI for Email Service"
+  sensitive   = true
+}

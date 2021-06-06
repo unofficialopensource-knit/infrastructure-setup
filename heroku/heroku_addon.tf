52db41c5b4ce1f2335b08712bcf80b52dd9c3377
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-06 10:09:56 +0530

    Updates the addon object name in terraform

--- heroku/heroku_addon.tf
+++ heroku/heroku_addon.tf
@@ -8,7 +8,7 @@ resource "heroku_addon" "email_service_redis_addon" {
   plan = "heroku-redis:hobby-dev"
 }
 
-resource "heroku_addon" "user_management_service_redis_addon" {
+resource "heroku_addon" "profile_service_redis_addon" {
   app  = heroku_app.user_management_service_app.name
   plan = "heroku-redis:hobby-dev"
 }

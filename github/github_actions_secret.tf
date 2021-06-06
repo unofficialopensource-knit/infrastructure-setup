7d30d5907f18f6529e09353b4f448f98fcc1ea88
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2021-06-06 11:26:10 +0530

    Removes hard-coded name (#36)

--- github/github_actions_secret.tf
+++ github/github_actions_secret.tf
@@ -1,11 +1,11 @@
 resource "github_actions_secret" "email_service_codecov_token" {
-  repository      = "email_service"
+  repository      = github_repository.email_service.name
   secret_name     = "CODECOV_TOKEN"
   plaintext_value = var.EMAIL_SERVICE_CODECOV_TOKEN
 }
 
 resource "github_actions_secret" "api_gateway_codecov_token" {
-  repository      = "api_gateway"
+  repository      = github_repository.api_gateway.name
   secret_name     = "CODECOV_TOKEN"
   plaintext_value = var.API_GATEWAY_CODECOV_TOKEN
 }

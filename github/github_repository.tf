409a1380e47136e0acf32a59698f3e95ffd8c7d1
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-08 22:18:10 +0530

    Adds auth_service repo

--- github/github_repository.tf
+++ github/github_repository.tf
@@ -14,6 +14,22 @@ resource "github_repository" "api_gateway" {
   auto_init              = false
 }
 
+resource "github_repository" "auth_service" {
+  name                   = "auth_service"
+  description            = "Codebase for auth_service"
+  visibility             = "public"
+  has_issues             = true
+  has_projects           = false
+  has_wiki               = false
+  is_template            = false
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_rebase_merge     = false
+  delete_branch_on_merge = true
+  has_downloads          = true
+  auto_init              = false
+}
+
 resource "github_repository" "email_service" {
   name                   = "email_service"
   description            = "Codebase for email_service"

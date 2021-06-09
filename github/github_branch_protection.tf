d8518cdf68671ae8eba0706f61afc9ef13fa02f2
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2021-06-09 23:00:38 +0530

    Adds branch protection rule for main branch in doscord_bot (#62)

--- github/github_branch_protection.tf
+++ github/github_branch_protection.tf
@@ -49,6 +49,20 @@ resource "github_branch_protection" "auth_service_branch_protection_release" {
   }
 }
 
+resource "github_branch_protection" "discord_bot_branch_protection_main" {
+  repository_id          = github_repository.discord_bot.node_id
+  pattern                = "main"
+  enforce_admins         = false
+  require_signed_commits = false
+  required_status_checks {
+    strict = true
+    contexts = [
+      "test",
+      "build"
+    ]
+  }
+}
+
 resource "github_branch_protection" "email_service_branch_protection_main" {
   repository_id          = github_repository.email_service.node_id
   pattern                = "main"

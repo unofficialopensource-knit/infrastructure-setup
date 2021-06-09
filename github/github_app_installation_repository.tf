aac2cdcdc32d94c17bf872e6eb7503296c0312f4
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2021-06-09 13:37:45 +0530

    Removes slack app integration (#55)

--- github/github_app_installation_repository.tf
+++ github/github_app_installation_repository.tf
@@ -87,33 +87,3 @@ resource "github_app_installation_repository" "wiki_deepsource_app" {
   installation_id = "16888192"
   repository      = github_repository.wiki.name
 }
-
-resource "github_app_installation_repository" "api_gateway_slack_app" {
-  installation_id = "16656997"
-  repository      = github_repository.api_gateway.name
-}
-
-resource "github_app_installation_repository" "email_service_slack_app" {
-  installation_id = "16656997"
-  repository      = github_repository.email_service.name
-}
-
-resource "github_app_installation_repository" "platform_setup_slack_app" {
-  installation_id = "16656997"
-  repository      = github_repository.platform_setup.name
-}
-
-resource "github_app_installation_repository" "profile_service_slack_app" {
-  installation_id = "16656997"
-  repository      = github_repository.profile_service.name
-}
-
-resource "github_app_installation_repository" "web_client_slack_app" {
-  installation_id = "16656997"
-  repository      = github_repository.web_client.name
-}
-
-resource "github_app_installation_repository" "wiki_slack_app" {
-  installation_id = "16656997"
-  repository      = github_repository.wiki.name
-}

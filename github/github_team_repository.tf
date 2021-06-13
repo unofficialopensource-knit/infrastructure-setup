e2164b0f23024f4d30381d7e8d28f0b12a526ecd
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-14 00:01:18 +0530

    Adds ops team in all other repos

--- github/github_team_repository.tf
+++ github/github_team_repository.tf
@@ -4,10 +4,22 @@ resource "github_team_repository" "api_gateway_backend_team" {
   permission = "maintain"
 }
 
+resource "github_team_repository" "api_gateway_ops_team" {
+  team_id    = github_team.ops_team.id
+  repository = github_repository.api_gateway.name
+  permission = "push"
+}
+
 resource "github_team_repository" "auth_service_backend_team" {
   team_id    = github_team.backend_team.id
   repository = github_repository.auth_service.name
   permission = "maintain"
+}
+
+resource "github_team_repository" "auth_service_ops_team" {
+  team_id    = github_team.ops_team.id
+  repository = github_repository.auth_service.name
+  permission = "push"
 }
 
 resource "github_team_repository" "discord_bot_ops_team" {
@@ -28,6 +40,12 @@ resource "github_team_repository" "email_service_backend_team" {
   permission = "maintain"
 }
 
+resource "github_team_repository" "email_service_ops_team" {
+  team_id    = github_team.ops_team.id
+  repository = github_repository.email_service.name
+  permission = "push"
+}
+
 resource "github_team_repository" "platform_setup_ops_team" {
   team_id    = github_team.ops_team.id
   repository = github_repository.platform_setup.name
@@ -40,10 +58,22 @@ resource "github_team_repository" "profile_service_backend_team" {
   permission = "maintain"
 }
 
+resource "github_team_repository" "profile_service_ops_team" {
+  team_id    = github_team.ops_team.id
+  repository = github_repository.profile_service.name
+  permission = "push"
+}
+
 resource "github_team_repository" "web_client_frontend_team" {
   team_id    = github_team.frontend_team.id
   repository = github_repository.web_client.name
   permission = "maintain"
+}
+
+resource "github_team_repository" "web_client_ops_team" {
+  team_id    = github_team.ops_team.id
+  repository = github_repository.web_client.name
+  permission = "push"
 }
 
 resource "github_team_repository" "wiki_ops_team" {

7b144d6e8b63d3de6bee9ec82488c2d240811340
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-27 00:56:55 +0530

    Fixes SH-2006

--- scripts/ci_utils.sh
+++ scripts/ci_utils.sh
@@ -1,13 +1,15 @@
 init_terraform() {
     for i in "$@"
     do
-        `terraform -chdir=$1 init -input=false`
+        echo "Initializing terraform for $i"
+        terraform -chdir=$1 init -input=false
     done
 }
 
 validate_terraform() {
     for i in "$@"
     do
-        `terraform -chdir=$1 validate -json`
+        echo "Validating terraform plan for $i"
+        terraform -chdir=$1 validate -json
     done
 }

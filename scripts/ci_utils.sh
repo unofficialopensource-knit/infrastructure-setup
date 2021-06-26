4df5b8aeb60380399bd8dee39bd7414dc14d0c5b
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-27 01:09:18 +0530

    Adds command for applying terraform plan

--- scripts/ci_utils.sh
+++ scripts/ci_utils.sh
@@ -21,3 +21,11 @@ plan_terraform() {
         terraform -chdir="$1" plan -input=false
     done
 }
+
+apply_terraform() {
+    for i in "$@"
+    do
+        echo "Applying terraform plan for" "$i"
+        terraform -chdir="$1" apply -auto-approve -input=false
+    done
+}

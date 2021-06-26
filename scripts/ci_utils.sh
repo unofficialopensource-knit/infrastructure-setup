930c2557219127dcbbf14d050a9232311364a22d
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-27 00:50:50 +0530

    Fixes SH-2068

--- scripts/ci_utils.sh
+++ scripts/ci_utils.sh
@@ -1,12 +1,12 @@
 init_terraform() {
-    for i in $@
+    for i in "$@"
     do
         `terraform -chdir=$1 init -input=false`
     done
 }
 
 validate_terraform() {
-    for i in $@
+    for i in "$@"
     do
         `terraform -chdir=$1 validate -json`
     done

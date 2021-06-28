3bc2badc5d8453d411ff6dbd8169a899d85a4e31
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-28 17:44:30 +0530

    Formatting commit

--- src/aws/container_registry.tf
+++ src/aws/container_registry.tf
@@ -1,10 +1,10 @@
 resource "aws_ecr_repository" "email_service" {
-    name = "email_service"
-    encryption_configuration {
-        encryption_type = "AES256"
-    }
-    image_tag_mutability = "IMMUTABLE"
-    image_scanning_configuration {
-      scan_on_push = true
-    }
+  name = "email_service"
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }

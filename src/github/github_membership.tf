a71ee182e192a7fa8dcdddd05969285d8b186d24
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-28 17:41:10 +0530

    Adds shreyasi as a member

--- src/github/github_membership.tf
+++ src/github/github_membership.tf
@@ -12,3 +12,8 @@ resource "github_membership" "prakhar_invite" {
   username = "Prakhar-Katiyar"
   role     = "member"
 }
+
+resource "github_membership" "shreyasi_invite" {
+  username = "Shreyasi03"
+  role     = "member"
+}
--- src/github/github_team_membership.tf
+++ src/github/github_team_membership.tf
@@ -28,6 +28,12 @@ resource "github_team_membership" "prakhar_data_membership" {
   role     = "member"
 }
 
+resource "github_team_membership" "shreyasi_data_membership" {
+  team_id  = github_team.data_team.id
+  username = "Shreyasi03"
+  role     = "member"
+}
+
 resource "github_team_membership" "pandeysambhi_front_end_membership" {
   team_id  = github_team.frontend_team.id
   username = "pandeysambhi"

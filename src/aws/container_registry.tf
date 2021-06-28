d4a7bc543f4c10c9ed0ff6856b924df21fb37e12
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-28 17:49:13 +0530

    Adds ECR repo for all services

--- src/aws/container_registry.tf
+++ src/aws/container_registry.tf
@@ -1,5 +1,38 @@
+resource "aws_ecr_repository" "api_gateway" {
+  name = "api_gateway"
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "auth_service" {
+  name = "auth_service"
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 resource "aws_ecr_repository" "email_service" {
   name = "email_service"
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "profile_service" {
+  name = "profile_service"
   encryption_configuration {
     encryption_type = "AES256"
   }

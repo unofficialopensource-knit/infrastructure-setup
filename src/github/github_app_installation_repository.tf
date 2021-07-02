379e7823748752a13e0aaabfef1ec19ad792c8fc
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-02 15:37:12 +0530

    Adds app for gambley backend

--- src/github/github_app_installation_repository.tf
+++ src/github/github_app_installation_repository.tf
@@ -18,6 +18,11 @@ resource "github_app_installation_repository" "email_service_codecov_app" {
   repository      = github_repository.email_service.name
 }
 
+resource "github_app_installation_repository" "gambley_backend_codecov_app" {
+  installation_id = "16927671"
+  repository      = github_repository.gambley_backend.name
+}
+
 resource "github_app_installation_repository" "profile_service_codecov_app" {
   installation_id = "16927671"
   repository      = github_repository.profile_service.name
@@ -46,6 +51,11 @@ resource "github_app_installation_repository" "discord_bot_codefactor_app" {
 resource "github_app_installation_repository" "email_service_codefactor_app" {
   installation_id = "16967095"
   repository      = github_repository.email_service.name
+}
+
+resource "github_app_installation_repository" "gambley_backend_codefactor_app" {
+  installation_id = "16967095"
+  repository      = github_repository.gambley_backend.name
 }
 
 resource "github_app_installation_repository" "platform_setup_codefactor_app" {
@@ -81,6 +91,11 @@ resource "github_app_installation_repository" "discord_bot_deepsource_app" {
 resource "github_app_installation_repository" "email_service_deepsource_app" {
   installation_id = "16888192"
   repository      = github_repository.email_service.name
+}
+
+resource "github_app_installation_repository" "gambley_backend_deepsource_app" {
+  installation_id = "16888192"
+  repository      = github_repository.gambley_backend.name
 }
 
 resource "github_app_installation_repository" "platform_setup_deepsource_app" {

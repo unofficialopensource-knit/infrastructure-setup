e51fdda92b75545715173854ce28c9360d9a41e2
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-02 15:37:52 +0530

    Adds team membership for gambley backend

--- src/github/github_team_repository.tf
+++ src/github/github_team_repository.tf
@@ -46,6 +46,18 @@ resource "github_team_repository" "email_service_ops_team" {
   permission = "push"
 }
 
+resource "github_team_repository" "gambley_backend_backend_team" {
+  team_id    = github_team.backend_team.id
+  repository = github_repository.gambley_backend.name
+  permission = "maintain"
+}
+
+resource "github_team_repository" "gambley_backend_ops_team" {
+  team_id    = github_team.ops_team.id
+  repository = github_repository.gambley_backend.name
+  permission = "push"
+}
+
 resource "github_team_repository" "platform_setup_ops_team" {
   team_id    = github_team.ops_team.id
   repository = github_repository.platform_setup.name

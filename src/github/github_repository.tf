323df86261e64dc4b27247a99a54acf82ee75c14
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2021-07-02 15:43:51 +0530

    Adds repo for gambley backend (#81)
    * Adds repo for gambley backend
    
    * Adds repo for gambley backend
    
    * Adds app for gambley backend
    
    * Adds team membership for gambley backend

--- src/github/github_repository.tf
+++ src/github/github_repository.tf
@@ -66,6 +66,23 @@ resource "github_repository" "email_service" {
   vulnerability_alerts   = true
 }
 
+resource "github_repository" "gambley_backend" {
+  name                   = "gambley_backend"
+  description            = "Codebase for gambley_backend"
+  visibility             = "public"
+  has_issues             = true
+  has_projects           = false
+  has_wiki               = false
+  is_template            = false
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_rebase_merge     = false
+  delete_branch_on_merge = true
+  has_downloads          = true
+  auto_init              = false
+  vulnerability_alerts   = true
+}
+
 resource "github_repository" "platform_setup" {
   name                   = "platform_setup"
   description            = "Codebase for platform_setup"

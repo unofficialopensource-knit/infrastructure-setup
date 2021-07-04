718328615e9a8088caca3deed6f821f650a4e2a9
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-04 12:38:48 +0530

    Adds validation command in pipeline

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,7 +1,7 @@
 init_terraform() {
     for dir in "$@"
     do
-        echo "Initializing terraform for" "$i"
+        echo "Initializing terraform for" "$dir"
         docker container run \
             --env AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
             --env AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
@@ -15,7 +15,7 @@ init_terraform() {
 format_terraform() {
     for dir in "$@"
     do
-        echo "Initializing terraform for" "$i"
+        echo "Initializing terraform for" "$dir"
         docker container run \
             --env AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
             --env AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
@@ -23,5 +23,19 @@ format_terraform() {
             --volume github-provider:/app/src/github/.terraform \
             --volume heroku-provider:/app/src/heroku/.terraform \
             infrastructure:"$GITHUB_COMMIT_SHA" -chdir=src/"$dir" fmt -check
+    done
+}
+
+validate_terraform() {
+    for dir in "$@"
+    do
+        echo "Initializing terraform for" "$dir"
+        docker container run \
+            --env AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
+            --env AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
+            --volume aws-provider:/app/src/aws/.terraform \
+            --volume github-provider:/app/src/github/.terraform \
+            --volume heroku-provider:/app/src/heroku/.terraform \
+            infrastructure:"$GITHUB_COMMIT_SHA" -chdir=src/"$dir" validate -json
     done
 }

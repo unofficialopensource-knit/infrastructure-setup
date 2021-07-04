af3a3fc1b0ae0a4cf237285f470e78caa59aec22
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-04 12:17:32 +0530

    Adds init step with volume supprt

---
+++ scripts/utils.sh
@@ -0,0 +1,29 @@
+init_terraform() {
+    for dir in "$@"
+    do
+        echo "Initializing terraform for" "$i"
+        docker container run \
+            --env AWS_ACCESS_KEY_ID=${{ secrets.AWS_ACCESS_KEY_ID }} \
+            --env AWS_SECRET_ACCESS_KEY=${{ secrets.AWS_SECRET_ACCESS_KEY }} \
+            --name infrastructure \
+            --volume aws-provider:/app/src/aws/.terraform \
+            --volume github-provider:/app/src/github/.terraform \
+            --volume heroku-provider:/app/src/heroku/.terraform \
+            infrastructure:${{ github.sha }} -chdir=src/"$dir" init -input=false
+    done
+}
+
+format_terraform() {
+    for dir in "$@"
+    do
+        echo "Initializing terraform for" "$i"
+        docker container run \
+            --env AWS_ACCESS_KEY_ID=${{ secrets.AWS_ACCESS_KEY_ID }} \
+            --env AWS_SECRET_ACCESS_KEY=${{ secrets.AWS_SECRET_ACCESS_KEY }} \
+            --name infrastructure \
+            --volume aws-provider:/app/src/aws/.terraform \
+            --volume github-provider:/app/src/github/.terraform \
+            --volume heroku-provider:/app/src/heroku/.terraform \
+            infrastructure:${{ github.sha }} -chdir=src/"$dir" fmt -check
+    done
+}

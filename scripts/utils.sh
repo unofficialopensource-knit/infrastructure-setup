4ea08bb5566c459e150cdf3392ac204c210c01a7
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-04 12:27:55 +0530

    Formatting check in test job

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -12,17 +12,16 @@ init_terraform() {
     done
 }
 
-# format_terraform() {
-#     for dir in "$@"
-#     do
-#         echo "Initializing terraform for" "$i"
-#         docker container run \
-#             --env AWS_ACCESS_KEY_ID=${{ secrets.AWS_ACCESS_KEY_ID }} \
-#             --env AWS_SECRET_ACCESS_KEY=${{ secrets.AWS_SECRET_ACCESS_KEY }} \
-#             --name infrastructure \
-#             --volume aws-provider:/app/src/aws/.terraform \
-#             --volume github-provider:/app/src/github/.terraform \
-#             --volume heroku-provider:/app/src/heroku/.terraform \
-#             infrastructure:${{ github.sha }} -chdir=src/"$dir" fmt -check
-#     done
-# }
+format_terraform() {
+    for dir in "$@"
+    do
+        echo "Initializing terraform for" "$i"
+        docker container run \
+            --env AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
+            --env AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
+            --volume aws-provider:/app/src/aws/.terraform \
+            --volume github-provider:/app/src/github/.terraform \
+            --volume heroku-provider:/app/src/heroku/.terraform \
+            infrastructure:"$GITHUB_COMMIT_SHA" -chdir=src/"$dir" fmt -check
+    done
+}

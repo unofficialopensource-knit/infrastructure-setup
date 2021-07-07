66eb8b66a9b598c1b298e7ac1a4b993c999c3d94
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-08 00:11:37 +0530

    Adds a public subnet to place NAT gateway

--- src/aws/subnet.tf
+++ src/aws/subnet.tf
@@ -1,6 +1,13 @@
-resource "aws_subnet" "gambley_subnet" {
+resource "aws_subnet" "gambley_subnet_private" {
   vpc_id                  = aws_vpc.gambley_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = "false"
+}
+
+resource "aws_subnet" "gambley_subnet_public" {
+  vpc_id                  = aws_vpc.gambley_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = "true"
 }

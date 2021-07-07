4024cb727bef142a4eff75b3621263f7fda3df00
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2021-07-08 01:33:56 +0530

    Adds subnets (#93)

--- src/aws/subnet.tf
+++ src/aws/subnet.tf
@@ -1,13 +1,21 @@
-# resource "aws_subnet" "gambley_subnet_private" {
-#   vpc_id                  = aws_vpc.gambley_vpc.id
-#   cidr_block              = "10.0.1.0/24"
-#   availability_zone       = "ap-south-1a"
-#   map_public_ip_on_launch = "false"
-# }
+resource "aws_subnet" "gambley_subnet_private" {
+  vpc_id                  = aws_vpc.gambley_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = "false"
+  tags = {
+    "Name"      = "Gambley Private Subnet"
+    "Terraform" = "True"
+  }
+}
 
-# resource "aws_subnet" "gambley_subnet_public" {
-#   vpc_id                  = aws_vpc.gambley_vpc.id
-#   cidr_block              = "10.0.2.0/24"
-#   availability_zone       = "ap-south-1a"
-#   map_public_ip_on_launch = "true"
-# }
+resource "aws_subnet" "gambley_subnet_public" {
+  vpc_id                  = aws_vpc.gambley_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = "true"
+  tags = {
+    "Name"      = "Gambley Public Subnet"
+    "Terraform" = "True"
+  }
+}

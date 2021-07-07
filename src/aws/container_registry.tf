e6ed985ad6ea384b9fe0b8289ea6a9abeb58095b
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2021-07-07 22:42:26 +0530

    Adds ecr repo for gambley_backend (#90)

--- src/aws/container_registry.tf
+++ src/aws/container_registry.tf
@@ -31,6 +31,17 @@ resource "aws_ecr_repository" "email_service" {
   }
 }
 
+resource "aws_ecr_repository" "gambley_backend" {
+  name = "gambley_backend"
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 resource "aws_ecr_repository" "profile_service" {
   name = "profile_service"
   encryption_configuration {

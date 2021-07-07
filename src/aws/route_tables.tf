e9abb3d8d89da54ffe296a48b44b0cf0bb82e505
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2021-07-08 00:32:27 +0530

    Adds Network layer for gambley (#91)
    * Adds VPC for gambley
    
    * Refactor commit
    
    * Adds private subnet for gambley vpc
    
    * Adds internet gateway in VPC
    
    * Refactor commit
    
    * Adds a public subnet to place NAT gateway
    
    * Adds an elastic IP for NAT Gateway
    
    * Adds a NAT Gateway
    
    * Refactor commit
    
    * Removes tags from NAT Gateway
    
    * Adds private route table with association
    
    * Refactor commit
    
    * Adds public\ route table with association

---
+++ src/aws/route_tables.tf
@@ -0,0 +1,25 @@
+resource "aws_route_table" "gambley_private_route_table" {
+  vpc_id = aws_vpc.gambley_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.gambley_nat_gateway.id
+  }
+}
+
+resource "aws_route_table" "gambley_public_route_table" {
+  vpc_id = aws_vpc.gambley_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gambley_internet_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "gambley_private_route_table_association" {
+  subnet_id      = aws_subnet.gambley_subnet_private.id
+  route_table_id = aws_route_table.gambley_private_route_table.id
+}
+
+resource "aws_route_table_association" "gambley_public_route_table_association" {
+  subnet_id      = aws_subnet.gambley_subnet_public.id
+  route_table_id = aws_route_table.gambley_public_route_table.id
+}

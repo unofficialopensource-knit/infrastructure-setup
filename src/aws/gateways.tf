d612c9b8b2442e46a855e441337c3e6483b76e40
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-08 00:15:13 +0530

    Adds a NAT Gateway

--- src/aws/gateways.tf
+++ src/aws/gateways.tf
@@ -1,3 +1,12 @@
 resource "aws_internet_gateway" "gambley_internet_gateway" {
   vpc_id = aws_vpc.gambley_vpc.id
 }
+
+resource "aws_nat_gateway" "gambley_nat_gateway" {
+    allocation_id = aws_eip.gambley_nat_eip.id
+    subnet_id = aws_subnet.gambley_subnet_public.id
+    tags = {
+        "Name" = "Rinnegan NAT Gateway 1A"
+        "Terraform" = "True"
+    }
+}

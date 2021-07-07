3701413fe451165f04d61346a0b9681f17bafcfb
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-08 00:21:00 +0530

    Removes tags from NAT Gateway

--- src/aws/gateways.tf
+++ src/aws/gateways.tf
@@ -5,8 +5,4 @@ resource "aws_internet_gateway" "gambley_internet_gateway" {
 resource "aws_nat_gateway" "gambley_nat_gateway" {
   allocation_id = aws_eip.gambley_nat_eip.id
   subnet_id     = aws_subnet.gambley_subnet_public.id
-  tags = {
-    "Name"      = "Rinnegan NAT Gateway 1A"
-    "Terraform" = "True"
-  }
 }

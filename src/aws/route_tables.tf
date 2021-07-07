27b8ceb4eceef450156e49cb822fc33afdad694b
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-08 00:21:40 +0530

    Refactor commit

--- src/aws/route_tables.tf
+++ src/aws/route_tables.tf
@@ -1,12 +1,12 @@
 resource "aws_route_table" "gambley_private_route_table" {
-    vpc_id = aws_vpc.gambley_vpc.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_nat_gateway.gambley_nat_gateway.id
-    }
+  vpc_id = aws_vpc.gambley_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.gambley_nat_gateway.id
+  }
 }
 
 resource "aws_route_table_association" "gambley_private_route_table_association" {
-    subnet_id = aws_subnet.gambley_subnet_private.id
-    route_table_id = aws_route_table.gambley_private_route_table.id
+  subnet_id      = aws_subnet.gambley_subnet_private.id
+  route_table_id = aws_route_table.gambley_private_route_table.id
 }

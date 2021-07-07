53fbbdf2aeb050c97b4bab256f45f6355680395b
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-08 02:24:09 +0530

    Adds tags for internet gateway and NAT gateway

--- src/aws/gateways.tf
+++ src/aws/gateways.tf
@@ -1,8 +1,16 @@
 resource "aws_internet_gateway" "gambley_internet_gateway" {
   vpc_id = aws_vpc.gambley_vpc.id
+  tags = {
+    "Name"      = "Gambley Internet Gateway"
+    "Terraform" = "True"
+  }
 }
 
 resource "aws_nat_gateway" "gambley_nat_gateway" {
   allocation_id = aws_eip.gambley_nat_eip.id
   subnet_id     = aws_subnet.gambley_subnet_public.id
+  tags = {
+    "Name"      = "Gambley NAT Gateway"
+    "Terraform" = "True"
+  }
 }

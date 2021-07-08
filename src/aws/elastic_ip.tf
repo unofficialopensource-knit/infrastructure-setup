1442cf54656e21ba91e6518ac6c74be4dfd07dae
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2021-07-08 21:48:33 +0530

    Adds EC2 instance for master node (#98)
    * Adds elastic IP
    
    * Sets up data download in pipeline (#99)
    
    * Adds data dir to git ignore
    
    * Adds data setup command
    
    * Updates pipeline to run for current PR
    
    * Updates pipeline to run for current PR
    
    * Downgrades ubuntu version and upgrades the step in build job
    
    * Downgrades ubuntu version and upgrades the step in deploy job
    
    * Reverts pipeline run conditions
    
    * Adds ssh key pari
    
    * Adds data setup in test step of pipeline
    
    * Updates ubuntu version in test tep
    
    * Adds security group
    
    * Refactor commit
    
    * Adds ec2 instance
    
    * Adds tags for EC2 instance
    
    * Adds instance id for elastic IP

--- src/aws/elastic_ip.tf
+++ src/aws/elastic_ip.tf
@@ -1,1 +1,7 @@
-resource "aws_eip" "gambley_node1_eip" {}
+resource "aws_eip" "gambley_node1_eip" {
+  instance = aws_instance.gambley_swarm_master.id
+  tags = {
+    "Name"      = "Gambley Node1 Elastic IP"
+    "Terraform" = "True"
+  }
+}

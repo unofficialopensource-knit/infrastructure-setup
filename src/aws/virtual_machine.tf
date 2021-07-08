ec599a82f7e255586b122fed4adea349a5643b9a
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-08 21:20:24 +0530

    Adds security group

--- src/aws/virtual_machine.tf
+++ src/aws/virtual_machine.tf
@@ -1,9 +1,32 @@
 # resource "aws_instance" "gambley_swarm_master" {
 #   ami           = "ami-0c1a7f89451184c8b"
 #   instance_type = "t3a.small"
+#   key_name = aws_key_pair.gambley_ssh_key.key_name
+#   associate_public_ip_address = true
+#   security_groups = ""
 # }
 
 resource "aws_key_pair" "gambley_ssh_key" {
   key_name   = "Gambley SSH Key"
   public_key = file("${path.module}/data/id_gambley.pub")
+}
+
+resource "aws_security_group" "gambley_security_group" {
+  name = "Gambley Security group"
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
 }

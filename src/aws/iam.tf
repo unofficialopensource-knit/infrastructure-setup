a01fc47986b83e1436e4bd22edc7dad6b9fa0e4d
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-10 01:01:13 +0530

    Adds IAM role for instance

---
+++ src/aws/iam.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_role" "gambley_instance_role" {
+  name = "gambley instance role"
+
+  assume_role_policy = file("${path.module}/iam_role_policy.json")
+
+  tags = {
+      "Terraform" = "True"
+  }
+}
+
+# resource "aws_iam_instance_profile" "gambley_iam_instance_profile" {
+#     name = "gambley iam profile"
+#     role = aws_iam_role.gambley_instance_role.name
+# }

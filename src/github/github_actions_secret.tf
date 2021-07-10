f9e830b9e2df3c9584ef31d89c98714bf17f56cb
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-10 14:33:45 +0530

    Adds secret in github repo

--- src/github/github_actions_secret.tf
+++ src/github/github_actions_secret.tf
@@ -15,3 +15,21 @@ resource "github_actions_secret" "gambley_backend_deepsource_dsn" {
   secret_name     = "DEEPSOURCE_DSN"
   plaintext_value = var.GAMBLEY_BACKEND_DEEPSOURCE_DSN
 }
+
+resource "github_actions_secret" "gambley_backend_mail_username" {
+  repository      = github_repository.gambley_backend.name
+  secret_name     = "MAIL_USERNAME"
+  plaintext_value = var.GAMBLEY_BACKEND_MAIL_USERNAME
+}
+
+resource "github_actions_secret" "gambley_backend_mail_password" {
+  repository      = github_repository.gambley_backend.name
+  secret_name     = "MAIL_PASSWORD"
+  plaintext_value = var.GAMBLEY_BACKEND_MAIL_PASSWORD
+}
+
+resource "github_actions_secret" "gambley_backend_supress_send" {
+  repository      = github_repository.gambley_backend.name
+  secret_name     = "SUPPRESS_SEND"
+  plaintext_value = var.GAMBLEY_BACKEND_SUPPRESS_SEND
+}

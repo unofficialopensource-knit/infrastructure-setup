963ff0f3d7cb7b93a542495652acbbf4e19388ff
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2021-07-10 14:58:46 +0530

    Deprecates email_service from heroku (#107)
    * Removes postgres database from email_service
    
    * Removes redis queue from email_service
    
    * Removes secrets from var file
    
    * Removes email_srevice app from heroku
    
    * Removes config vars for email_service
    
    * Removes config vars for email_service from pipeline

--- src/heroku/heroku_addon.tf
+++ src/heroku/heroku_addon.tf
@@ -1,13 +1,3 @@
-resource "heroku_addon" "email_service_database_addon" {
-  app  = heroku_app.email_service_app.name
-  plan = "heroku-postgresql:hobby-dev"
-}
-
-resource "heroku_addon" "email_service_redis_addon" {
-  app  = heroku_app.email_service_app.name
-  plan = "heroku-redis:hobby-dev"
-}
-
 resource "heroku_addon" "profile_service_redis_addon" {
   app  = heroku_app.profile_service_app.name
   plan = "heroku-redis:hobby-dev"
--- src/heroku/vars.tf
+++ src/heroku/vars.tf
@@ -9,36 +9,6 @@ variable "HEROKU_API_KEY" {
   sensitive   = true
 }
 
-variable "EMAIL_SERVICE_DATABASE_URL" {
-  type        = string
-  description = "Database URI for Email Service"
-  sensitive   = true
-}
-
-variable "EMAIL_SERVICE_MAIL_PASSWORD" {
-  type        = string
-  description = "Email password for Email Service"
-  sensitive   = true
-}
-
-variable "EMAIL_SERVICE_MAIL_USERNAME" {
-  type        = string
-  description = "Email address for Email Service"
-  sensitive   = true
-}
-
-variable "EMAIL_SERVICE_REDIS_TLS_URL" {
-  type        = string
-  description = "Redis(secure) URI for Email Service"
-  sensitive   = true
-}
-
-variable "EMAIL_SERVICE_REDIS_URL" {
-  type        = string
-  description = "Redis URI for Email Service"
-  sensitive   = true
-}
-
 variable "PROFILE_SERVICE_DATABASE_URL" {
   type        = string
   description = "Database URI for Profile Service"

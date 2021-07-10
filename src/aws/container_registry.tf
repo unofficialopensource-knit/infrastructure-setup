2ce49105476b3366a35d9ba0dc75fce891bdf404
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2021-07-10 15:23:21 +0530

    Removes ECR repo (#109)

--- src/aws/container_registry.tf
+++ src/aws/container_registry.tf
@@ -20,17 +20,6 @@ resource "aws_ecr_repository" "auth_service" {
   }
 }
 
-resource "aws_ecr_repository" "email_service" {
-  name = "email_service"
-  encryption_configuration {
-    encryption_type = "AES256"
-  }
-  image_tag_mutability = "IMMUTABLE"
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
 resource "aws_ecr_repository" "gambley_backend" {
   name = "gambley_backend"
   encryption_configuration {

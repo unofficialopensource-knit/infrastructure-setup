e0538fd949df79c78ba9e4a228844d6ddffcb3eb
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-10 15:04:18 +0530

    Removes github action secrets for email_service

--- src/github/github_actions_secret.tf
+++ src/github/github_actions_secret.tf
@@ -1,9 +1,3 @@
-resource "github_actions_secret" "email_service_codecov_token" {
-  repository      = github_repository.email_service.name
-  secret_name     = "CODECOV_TOKEN"
-  plaintext_value = var.EMAIL_SERVICE_CODECOV_TOKEN
-}
-
 resource "github_actions_secret" "api_gateway_codecov_token" {
   repository      = github_repository.api_gateway.name
   secret_name     = "CODECOV_TOKEN"

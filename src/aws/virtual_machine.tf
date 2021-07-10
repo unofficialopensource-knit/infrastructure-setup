b2af05ca56306bf6e80054087e03e136abb37a31
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-10 11:47:00 +0530

    Removes duplicates

--- src/aws/virtual_machine.tf
+++ src/aws/virtual_machine.tf
@@ -6,8 +6,6 @@ resource "aws_instance" "gambley_swarm_master" {
   user_data                   = file("${path.module}/data/user_data.sh")
   security_groups             = [aws_security_group.gambley_security_group.name]
   iam_instance_profile        = aws_iam_instance_profile.gambley_iam_instance_profile.name
-  user_data                   = file("${path.module}/data/user_data.sh")
-  security_groups             = [aws_security_group.gambley_security_group.name]
   tags = {
     "Name"      = "Gambley Node1"
     "Terraform" = "True"

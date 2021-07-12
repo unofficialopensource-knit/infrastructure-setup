97610c062119ecfc524a9c5de4b622c0fa29e933
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-12 14:34:44 +0530

    Adds security group rule for ec2

--- src/aws/virtual_machine.tf
+++ src/aws/virtual_machine.tf
@@ -39,4 +39,12 @@ resource "aws_security_group" "gambley_security_group" {
       "0.0.0.0/0"
     ]
   }
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
 }

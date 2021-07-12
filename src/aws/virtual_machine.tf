e2cc71ef25c9e0a61010a4a7f45f8f7b958eac72
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2021-07-12 23:22:08 +0530

    Adds security group rule for 443 port (#112)

--- src/aws/virtual_machine.tf
+++ src/aws/virtual_machine.tf
@@ -47,4 +47,12 @@ resource "aws_security_group" "gambley_security_group" {
       "0.0.0.0/0"
     ]
   }
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
 }

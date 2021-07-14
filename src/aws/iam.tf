c8b62b7df8d4cc30a413cf8a43a4b1cc01f7e045
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-14 22:26:05 +0530

    Renames IAM roles and policies

--- src/aws/iam.tf
+++ src/aws/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "gambley_instance_role" {
-  name               = "gambley-instance-role"
+  name               = "GambleyInstanceRole"
   assume_role_policy = file("${path.module}/data/iam_role_policy.json")
   tags = {
     "Terraform" = "True"
@@ -7,7 +7,7 @@ resource "aws_iam_role" "gambley_instance_role" {
 }
 
 resource "aws_iam_instance_profile" "gambley_iam_instance_profile" {
-  name = "gambley-iam-profile"
+  name = "GambleyIAMProfile"
   role = aws_iam_role.gambley_instance_role.name
   tags = {
     "Terraform" = "True"
@@ -15,7 +15,7 @@ resource "aws_iam_instance_profile" "gambley_iam_instance_profile" {
 }
 
 resource "aws_iam_role_policy" "gambley_iam_instance_policy" {
-  name   = "gambley-iam-instance-policy"
+  name   = "GambleyIAMInstancePolicy"
   role   = aws_iam_role.gambley_instance_role.id
   policy = file("${path.module}/data/iam_role_instance_policy.json")
 }

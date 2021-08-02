7f3fd81b82d075d42ba69d20b612e0c0939575ea
Mayank Pathak <onlinejudge95@gmail.com>
2021-08-02 20:21:18 +0530

    Adds bastion VM

--- src/aws/virtual_machine.tf
+++ src/aws/virtual_machine.tf
@@ -16,6 +16,19 @@ resource "aws_instance" "gambley_swarm_master" {
   }
 }
 
+resource "aws_instance" "gambley_bastion_node" {
+  ami                         = "ami-0c1a7f89451184c8b"
+  instance_type               = "t3a.small"
+  key_name                    = aws_key_pair.gambley_ssh_key.key_name
+  associate_public_ip_address = true
+  user_data                   = file("${path.module}/data/user_data/install_nginx.sh")
+  security_groups             = [aws_security_group.gambley_bastion_security_group.name]
+  tags = {
+    "Name"      = "Gambley Bastion Node"
+    "Terraform" = "True"
+  }
+}
+
 resource "aws_key_pair" "gambley_ssh_key" {
   key_name   = "Gambley SSH Key"
   public_key = file("${path.module}/data/id_gambley.pub")
@@ -24,6 +37,43 @@ resource "aws_key_pair" "gambley_ssh_key" {
 resource "aws_security_group" "gambley_security_group" {
   name        = "Gambley Security group"
   description = "Security group for gambley host"
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+}
+
+resource "aws_security_group" "gambley_bastion_security_group" {
+  name        = "Gambley Security group"
+  description = "Security group for gambley bastion host"
   egress {
     from_port = 0
     to_port   = 0
